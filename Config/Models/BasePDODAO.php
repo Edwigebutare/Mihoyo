2d22a02ef9aa41d68d374237b34c0e596b68ae6b
Chance BUTARE <cb335968@iut21.u-bourgogne.fr>
2026-01-05 11:22:09 +0100

    création de la classe BasePDODAO pour l'accès PDO

---
+++ Config/Models/BasePDODAO.php
@@ -0,0 +1,47 @@
+<?php
+namespace Models;
+
+class BasePDODAO {
+    private PDO $db;
+
+     // Instancie et retourne la connexion PDO
+    private function getDB() : PDO {
+        
+          if ($this->db === null) {
+            try {
+                $this->db = new PDO(
+                    Config::get('dsn'),
+                    Config::get('user'),
+                    Config::get('pass')
+                );
+
+                $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+            } catch (PDOException $e) {
+                die('Erreur de connexion à la base de données : ' . $e->getMessage());
+            }
+        }
+
+        return $this->db;
+    }
+
+    // Exécute une requête SQL 
+    protected function execRequest(string $sql, array $params = null): PDOStatement|false {
+        $stmt = $this->getDB()->prepare($sql);
+
+        if ($stmt === false) {
+            return false;
+        }
+
+        if ($params !== null) {
+            $stmt->execute($params);
+        } else {
+            $stmt->execute();
+        }
+
+        return $stmt;
+    }
+}
+
+
